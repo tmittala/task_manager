d0208ded3e2edf1e5a74909149f06ab9752e1d05
tmittala <atushar52@gmail.com>
2024-04-12 12:41:11 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tushar'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+
+db = SQLAlchemy()
+db.init_app(app)
+
+from routes import *
+
+if __name__ == '__main__':
+    # with app.app_context():
+    #     db.create_all()  # This line creates the database with all the tables
+    app.run(debug=True)
